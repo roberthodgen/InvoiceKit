7ccf9e3af9885b1c123b7ef37ff2a2608555e21a
Rob Hodgen <rhodgen@hhsterling.com>
2025-07-29 12:01:52 -0400

    WIP WithTextBlock Fluent API

--- src/Tests/Pdf/TextBlockTests.cs
+++ src/Tests/Pdf/TextBlockTests.cs
@@ -18,52 +18,43 @@ public class TextBlockTests(ITestOutputHelper testOutputHelper)
         using var builder = PdfDocument.UsLetter;
         var pdfBytes = builder
             .DisplayLayoutGuidelines()
-            .AddBlock(doc => new TextBlock(doc.DefaultTextStyle)
-                .AddLine("Test Document", text => text.Font("Open Sans/Bold").FontSize(24f))
+            .AddTextBlockBlock(text => text
+                .AddLine("Test Document", line => line.Font("Open Sans/Bold").FontSize(24f))
                 .AddLine(
                     "Sample document for text block layout and rendering.",
-                    text => text.Color(SKColors.DimGray)))
-            .AddBlock(doc => new TextBlock(doc.DefaultTextStyle)
-                .AddLine("Default: The quick brown fox jumps over the lazy dog."))
-            .AddBlock(_ => new TextBlock(
-                new TextStyle
-                {
-                    ParagraphSpacing = new ParagraphSpacing
+                    line => line.Color(SKColors.DimGray)))
+            .AddTextBlock(text => text.AddLine("Default: The quick brown fox jumps over the lazy dog."))
+            .AddTextBlock(text => text
+                .ParagaphSpacing(
+                    new ParagraphSpacing
                     {
                         After = 2f,
-                    },
-                }).AddLine("Spacing after: The quick brown fox jumps over the lazy dog."))
-            .AddBlock(_ => new TextBlock(
-                    new TextStyle
-                    {
-                        LineHeight = 1f,
-                        ParagraphSpacing = new ParagraphSpacing
-                        {
-                            Before = 1f,
-                            After = 1f,
-                        },
                     })
+                .AddLine("Spacing after: The quick brown fox jumps over the lazy dog."))
+            .AddTextBlock(text => text
+                .ParagaphSpacing(
+                    new ParagraphSpacing
+                    {
+                        Before = 1f,
+                        After = 1f,
+                    })
+                .LineHeight(1f)
                 .AddLine("None: The quick brown fox jumps over the lazy dog."))
-            .AddBlock(_ => new TextBlock(
-                new TextStyle
-                {
-                    ParagraphSpacing = new ParagraphSpacing
+            .AddTextBlock(text => text
+                .ParagaphSpacing(
+                    new ParagraphSpacing
                     {
                         Before = 2f,
                         After = 1f,
-                    },
-                }).AddLine("Spacing before: The quick brown fox jumps over the lazy dog."))
-            .AddBlock(_ => new TextBlock(
-                new TextStyle
-                {
-                    LineHeight = 2f,
-                }).AddLine("Double line: The quick brown fox jumps over the lazy dog."))
-            .AddBlock(doc => new TextBlock(doc.DefaultTextStyle)
-                .AddLine(
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer pharetra neque nec sapien pharetra gravida. Aenean quis sapien enim. In semper quis nisi laoreet sollicitudin. Morbi vehicula massa sed erat egestas tempus. Duis tincidunt odio elit, a efficitur est dictum quis. Cras egestas ante et mi vulputate, in dapibus nisi suscipit. Sed sodales nibh leo, eu hendrerit nibh semper non. Praesent id nunc sed eros aliquam tristique eget ut erat. Pellentesque dignissim mattis justo sed viverra."))
-            .AddBlock(doc => new TextBlock(doc.DefaultTextStyle)
-                .AddLine(
-                    "Aenean dui mauris, sagittis ac dapibus et, commodo sit amet tortor. Vestibulum porttitor feugiat sem, at fermentum ex laoreet at. Curabitur lobortis finibus tincidunt. Morbi mattis quam nec nulla dapibus luctus. Quisque mattis nunc risus, quis pellentesque felis molestie ut. Donec rhoncus accumsan aliquam. Nulla vestibulum dolor eget sapien aliquet, a volutpat urna efficitur. Proin erat lorem, auctor placerat tincidunt eu, porta nec dui. Ut dapibus tortor sit amet tortor tristique, tempus pharetra nunc sagittis. Aenean pretium vulputate quam, at rutrum nisi commodo eu. Curabitur vitae erat nec lacus placerat suscipit iaculis et nunc."))
+                    })
+                .AddLine("Spacing before: The quick brown fox jumps over the lazy dog."))
+            .AddTextBlock(text => text
+                .LineHeight(2f)
+                .AddLine("Double line: The quick brown fox jumps over the lazy dog."))
+            .AddTextBlock(text => text
+                .AddLine("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer pharetra neque nec sapien pharetra gravida. Aenean quis sapien enim. In semper quis nisi laoreet sollicitudin. Morbi vehicula massa sed erat egestas tempus. Duis tincidunt odio elit, a efficitur est dictum quis. Cras egestas ante et mi vulputate, in dapibus nisi suscipit. Sed sodales nibh leo, eu hendrerit nibh semper non. Praesent id nunc sed eros aliquam tristique eget ut erat. Pellentesque dignissim mattis justo sed viverra."))
+            .AddTextBlock(text => text
+                .AddLine("Aenean dui mauris, sagittis ac dapibus et, commodo sit amet tortor. Vestibulum porttitor feugiat sem, at fermentum ex laoreet at. Curabitur lobortis finibus tincidunt. Morbi mattis quam nec nulla dapibus luctus. Quisque mattis nunc risus, quis pellentesque felis molestie ut. Donec rhoncus accumsan aliquam. Nulla vestibulum dolor eget sapien aliquet, a volutpat urna efficitur. Proin erat lorem, auctor placerat tincidunt eu, porta nec dui. Ut dapibus tortor sit amet tortor tristique, tempus pharetra nunc sagittis. Aenean pretium vulputate quam, at rutrum nisi commodo eu. Curabitur vitae erat nec lacus placerat suscipit iaculis et nunc."))
             .Build();
 
         stream.Write(pdfBytes);
