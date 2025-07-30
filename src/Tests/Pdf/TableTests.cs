495b19d28a97963e929d836aca188d7ed3a09f82
Erik Taylor <etaylor@hhsterling.com>
2025-07-30 16:50:54 -0400

    Created separate table box to simplify the api.

--- src/Tests/Pdf/TableTests.cs
+++ src/Tests/Pdf/TableTests.cs
@@ -17,11 +17,11 @@ public class TableTests(ITestOutputHelper testOutputHelper)
         using var builder = PdfDocument.UsLetter;
         var pdfBytes = builder
             // .DisplayLayoutGuidelines()
-            .AddBlock(doc => new TextBlock(doc.DefaultTextStyle)
+            .AddTextBlock(table => table
                 .AddLine("Simple Table", text => text.Font("Open Sans/Bold").FontSize(24f)))
-            .AddBlock(doc => new TextBlock(doc.DefaultTextStyle)
+            .AddTextBlock(table => table
                 .AddLine("The table below contains two rows and 3 equally spaced columns with row separators."))
-            .AddBlock(doc => new TableLayoutBuilder(doc.DefaultTextStyle)
+            .AddTableBlock(table => table
                 .UseEquallySpaceColumns()
                 .AddRowSeparators()
                 .AddHeader(header => header
@@ -35,11 +35,11 @@ public class TableTests(ITestOutputHelper testOutputHelper)
                     .AddCell(cell => cell.AddText("1"))
                     .AddCell(cell => cell.AddText("$ 20.00"))))
             // .AddPageBreak()
-            .AddBlock(doc => new TextBlock(doc.DefaultTextStyle)
+            .AddTextBlock(table => table
                 .AddLine("Fixed Width Columns", text => text.Font("Open Sans/Bold").FontSize(24f)))
-            .AddBlock(doc => new TextBlock(doc.DefaultTextStyle)
+            .AddTextBlock(table => table
                 .AddLine("The table below contains 3 fixed-percentage width columns."))
-            .AddBlock(doc => new TableLayoutBuilder(doc.DefaultTextStyle)
+            .AddTableBlock(table => table
                 .UseFixedColumnWidths(
                 [
                     ColumnWidthPercent.FromPercent(75),
@@ -59,11 +59,11 @@ public class TableTests(ITestOutputHelper testOutputHelper)
                         .AddCell(cell => cell.AddText("1"))
                         .AddCell(cell => cell.AddText("$ 20.00"))))
             // .AddPageBreak()
-            .AddBlock(doc => new TextBlock(doc.DefaultTextStyle)
+            .AddTextBlock(table => table
                 .AddLine("Consistent Row Heights", text => text.Font("Open Sans/Bold").FontSize(24f)))
-            .AddBlock(doc => new TextBlock(doc.DefaultTextStyle)
+            .AddTextBlock(table => table
                 .AddLine("All rows are the same height with row separators."))
-            .AddBlock(doc => new TableLayoutBuilder(doc.DefaultTextStyle)
+            .AddTableBlock(table => table
                 .UseFixedColumnWidths(
                 [
                     ColumnWidthPercent.FromPercent(50),
