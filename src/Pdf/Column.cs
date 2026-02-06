a98065eb20eca31eca7247618f0704c765335a1b
Erik Taylor <etaylor@hhsterling.com>
2026-02-06 17:16:37 -0500

    Made columns constructor internal.

--- src/Pdf/Column.cs
+++ src/Pdf/Column.cs
@@ -6,7 +6,7 @@ public sealed class Column
 
     public IColumnWidth ColumnWidth { get; }
 
-    public Column(ILayout layout, IColumnWidth columnWidth)
+    internal Column(ILayout layout, IColumnWidth columnWidth)
     {
         Layout = layout;
         ColumnWidth = columnWidth;
